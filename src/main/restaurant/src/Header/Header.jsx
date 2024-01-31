41b94b8539d0d9ac8664a19a5a0e9d97506d41d5
Woongchan Jeon <jchan71571@gmail.com>
2024-02-01 00:26:11 +0900

    0131-2

--- src/main/restaurant/src/Header/Header.jsx
+++ src/main/restaurant/src/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './css/Header.css'
 import HeaderLeft from './HeaderLeft';
 import { IoMenu } from "react-icons/io5";
@@ -6,7 +6,10 @@ import { IoMenu } from "react-icons/io5";
 export default function Header() {
     const HeaderMenuList = ['About', 'Menu', 'Contact', 'Social']
     const [headerLeftOn, setHeaderLeftOn] = useState(false)
-    const [headerMouseOn, setHeaderMouseOn] = useState(false)
+    const [headerMouseOn, setHeaderMouseOn] = useState(false) // 마우스가 헤더메뉴에 있음
+    const [moreMenuOn, setMoreMenuOn] = useState(false) // 마우스가 드롭다운 메뉴에 있음
+
+    const moreMenuRef = useRef(null)
 
     return (
         <div id='header'>
@@ -14,11 +17,11 @@ export default function Header() {
                 <a href="" className='header-logo-text'>WooDy</a>
             </div>
             <div className="header-menu">
-                <ul className='header-menu-list'>
+                <ul className='header-menu-list' onMouseEnter={()=>{setHeaderMouseOn(true)}} onMouseLeave={()=>{setHeaderMouseOn(false)}}>
                     {
                         HeaderMenuList.map((a,i)=>{
                             return(
-                                <HeaderMenu key={i} menu={a} headerMouseOn={headerMouseOn} setHeaderMouseOn={setHeaderMouseOn}/>
+                                <HeaderMenu key={i} menu={a} headerMouseOn={headerMouseOn}/>
                             )
                         })
                     }
@@ -27,8 +30,8 @@ export default function Header() {
             <div className="header-btn">
                 <IoMenu className='all-menu-btn' onClick={()=>{setHeaderLeftOn(!headerLeftOn)}}/>
             </div>
-            <div className="header-menu-more-container">
-                <div className='header-menu-more' style={{display: headerMouseOn ? 'block' : 'none'}} onMouseLeave={()=>{setHeaderMouseOn(false)}}>
+            <div className="header-menu-more-container" onMouseEnter={()=>{setHeaderMouseOn(true)}}>
+                <div className='header-menu-more' style={{display: headerMouseOn ? 'block' : 'none'}} >
                     <div className="more-menu-area" onMouseLeave={()=>{setHeaderMouseOn(false)}}></div>
                 </div>
             </div>
@@ -42,20 +45,14 @@ export default function Header() {
 function HeaderMenu(props) {
     const MoreMenuList = {'About':['레스토랑 소개', '쉐프 소개'], 'Menu':['Lunch', 'Dinner'], 'Contact':['공지사항', '예약', '오시는길'], 'Social':['인스타그램']}
 
-    const mouseEnterHandler = ()=>{
-        if(!props.HeaderMouseOn){
-            props.setHeaderMouseOn(true)
-        }
-    }
-
     return(
-        <li onMouseEnter={()=>{mouseEnterHandler()}}>
+        <li>
             <a href="" className='menu'>{props.menu}</a>
             <ul className='more-menu-list' style={{display: props.headerMouseOn ? '' : 'none'}} >
                 {
                     MoreMenuList[props.menu].map((a,i)=>{
                         return(
-                            <a href="" className='more-menu'>{a}</a>
+                            <a href="" className='more-menu' key={i}>{a}</a>
                         )
                     })
                 }
--- src/main/restaurant/src/Header/HeaderLeft.jsx
+++ src/main/restaurant/src/Header/HeaderLeft.jsx
@@ -18,8 +18,42 @@ export default function HeaderLeft(props) {
                         <div className="sign-in">로그인</div>
                         <div className="sign-up">회원가입</div>
                     </div>
-                    <div className="inner-middle"></div>
-                    <div className="inner-bottom"></div>
+                    <div className="line"></div>
+                    <div className="inner-middle">
+                        <div className="user-menu">
+                            <h5>Member</h5>
+                            <p>예약</p>
+                            <p>예약확인</p>
+                            <p>고객문의</p>
+                        </div>
+                        <div className="company">
+                            <h5>Restaurant</h5>
+                            <p>회사소개</p>
+                            <p>가맹점모집</p>
+                            <p>인재채용</p>
+                        </div>
+                        <div className="information">
+                            <h5>운영시간</h5>
+                            <div className="box">
+                                <h6>Open</h6>
+                                <p>AM10:30</p>
+                            </div>
+                            <div className="box">
+                                <h6>Close</h6>
+                                <p>PM9:30</p>
+                            </div>
+                            <div className="box">
+                                <h6>Break Time</h6>
+                                <p>PM3:30~PM5:30</p>
+                            </div>
+                            <div className="box">
+                                <p>매주 월요일 휴무</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="inner-bottom">
+                        <p>본 사이트는 포트폴리오 용도로 만들어진 사이트입니다.</p>
+                    </div>
                 </div>
             </div>
         </div>
