f1dea514d77e3464d7cef82348bdcf20c502a112
규식이 <hazekoi05@gmail.com>
2024-03-24 13:12:31 +0900

    "예약 셀렉트"

--- src/main/java/com/example/restaurant/repository/GuestRepository.java
+++ src/main/java/com/example/restaurant/repository/GuestRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface GuestRepository extends JpaRepository<Guest, Long> {
-  Page<Guest> findByGuestId(Long guestId, Pageable pageable);
+
 
   @Query("SELECT g.guest_id FROM Guest g WHERE g.phone = :phone")
-  Optional<Long> findByID(String phone);
+  Optional<Guest> findByID(String phone);
 }
--- src/main/java/com/example/restaurant/service/GuestService.java
+++ src/main/java/com/example/restaurant/service/GuestService.java
@@ -1,20 +1,53 @@
 package com.example.restaurant.service;
 
 
+import com.example.restaurant.dto.GReserveDTO;
 import com.example.restaurant.entity.Guest;
+import com.example.restaurant.repository.GreserveRepository;
 import com.example.restaurant.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class GuestService {
   @Autowired
   private GuestRepository guestRepository;
-  public Page<Guest> getGuestsByGuestId(Long guestId, int pageNumber, int pageSize) {
-    Pageable pageable = PageRequest.of(pageNumber, pageSize);
-    return guestRepository.findByGuestId(guestId, pageable);
+
+  @Autowired
+  private GreserveRepository greserveRepository;
+
+
+
+  public List<GReserveDTO> selectinfo(String phone, Guest guest) {
+
+
+
+
+    Optional<Guest> guestid = guestRepository.findByID(phone);
+
+
+
+
+    if (guestid.isPresent()) {
+      Guest guests = guestid.get();
+      // Guest 개체를 생성하여 값을 설정합니다.
+      Guest newGuest = new Guest(guest.getGuest_id(), phone);
+
+
+      List<GReserveDTO> guestsPage = greserveRepository.info(newGuest);
+      return guestsPage;
+
+    } else {
+      return null;
+    }
+
+
   }
 }
